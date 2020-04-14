c87a60be40a2beaeb2733ae11bc1541c06bffd3f
SebastianCalle <jusemonca@gmail.com>
2020-04-14 01:34:50 -0500

    [Feature]: Enable a channel layer to async

--- chatproject/chatproject/routing.py
+++ chatproject/chatproject/routing.py
@@ -1,6 +1,6 @@
+import chat.routing
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-import chat.routing
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
